dded0717f9dd5e55cc0098faf829682214662492
priyansh7124 <priyanshagrawal806@gmail.com>
2025-11-23 15:31:26 +0530

    Added Some Pie charts using chart js

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,16 +1,31 @@
+import AttendanceChart from "@/components/AttendanceChart";
+import CountChart from "@/components/CountCharts";
 import UserCard from "@/components/UserCard";
 
 const AdminPage = () => {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row">
-      <div className="w-full lg:w-2/3">
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
         {/* {USER CARDS} */}
-        <div className="flex gap-4 justify-between">
+        <div className="flex gap-4 justify-between flex-wrap">
           <UserCard type="student" />
           <UserCard type="teacher" />
           <UserCard type="parent" />
           <UserCard type="staff" />
         </div>
+        {/* Middle Charts */}
+        <div className="flex gap-4 flex-col lg:flex-row">
+          {/* Count Charts */}
+          <div className="w-full lg:w-1/3 h-[450px]">
+            <CountChart />
+          </div>
+          {/* Attendance Chart */}
+          <div className="w-full lg:w-2/3 h-[450px]">
+            <AttendanceChart />
+          </div>
+        </div>
+        {/* Bottom Charts */}
+        <div className=""></div>
       </div>
       <div className="w-full lg:w-1/3">r</div>
     </div>
