c174688f416bef3fd49fb2fb885ebb0d32ec1ce8
Priyansh Agrawal <priyanshagrawal806@gmail.com>
2025-12-17 14:17:59 +0530

    Changed the font-weight and font-style along with some slight error in Calendar

--- src/components/EventCalendar.tsx
+++ src/components/EventCalendar.tsx
@@ -14,7 +14,7 @@ const events = [
   {
     id: 1,
     title: "Lorem ipsum dolor",
-    time: "12:00 PM - 2:00 PM",
+    time: "10:00 PM - 2:00 PM",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   },
   {
